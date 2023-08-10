16104f4f1af6d136cd8a4d35e1cf0f244f490257
MichaelEWalker87 <Michael.E.Walker.87@gmail.com>
2023-08-10 13:21:01 -0600

    ProjectWarpUp

--- src/componets/SearchBar.js
+++ src/componets/SearchBar.js
@@ -1,5 +1,31 @@
-function SearchBar() {
-    return <div>SearchBar</div>;
+import './SearchBar.css'
+import { useState } from "react";
+
+
+function SearchBar({ onSubmit }) {
+    const [term, setTerm] = useState("");
+
+    const handleFromSubmit = (event) => {
+        event.preventDefault();
+        
+        onSubmit(term);
+    };
+    
+    const handleChange = (event) => {
+        setTerm(event.target.value);
+        //grabs the value from the keypress 
+    ;}
+
+    return(
+        <div className='search-bar'>
+            <form onSubmit={handleFromSubmit}>
+                <label>Enter Search Term</label>
+                <input value={term} onChange={handleChange}/> 
+            </form>
+        </div>
+    )
 }
 
 export default SearchBar;
+
+//form allows for a submit event, but needs event.prevent default allow for one event handler for enter and button click 
